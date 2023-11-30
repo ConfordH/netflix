5f0af8224a52ad7b126238ecf70f6c2d7573ebd6
ConfordH <hamiltonconford@gmail.com>
2023-11-30 14:38:36 +0300

    updates

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Inner } from "./styles/jumbotron";
+
+export default function Jumbotron() {
+  return (
+    <Inner direction={direction}>
+      <p>Hello Again!</p>
+    </Inner>
+  );
+}
